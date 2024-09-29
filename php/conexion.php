d3768503fb8d4a266a6c6648e069df6d471f89e6
flawless0011 <franciscopino1236@gmail.com>
2024-09-28 21:16:58 -0300

    2

---
+++ php/conexion.php
@@ -0,0 +1,23 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "usteam";
+
+$conexion = new mysqli($server, $user, $pass, $db);
+
+if($conexion->connect_errno){
+    die("Conexion Fallida" . $conexion->connect_errno);
+}
+
+else {
+    echo "Conectado";
+}
+
+?>
+
+
+
+
+
